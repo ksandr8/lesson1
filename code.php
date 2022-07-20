8c205d04b66e0203dc99205cbb667dea1e8ec3c4
ksandr8 <1@bag7!A>
2022-07-20 15:38:13 +0300

    Lesson 3. Task 1.

---
+++ code.php
@@ -0,0 +1,3 @@
+<?php
+//lesson 3. Task 1
+echo "привіт світ";
--- index.php
+++ index.php
@@ -1,2 +1,14 @@
+<meta charset="utf-8">
 <?php
-echo "Hello Git";
+
+//lesson 1. Task 1
+//echo "Hello World";
+
+//lesson 2. Task 1
+//echo "Hello Git";
+
+//lesson 3. Task 1
+error_reporting(E_ALL);
+ini_set('disable_errors', 'on');
+mb_internal_encoding('UTF-8');
+include 'code.php';
