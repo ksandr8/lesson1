4ccdde007281dceaeb5f2a4ed1f370b33662eb05
ksandr8 <1@bag7!A>
2022-07-21 17:29:09 +0300

    Lesson 3. Task 5.

--- code.php
+++ code.php
@@ -23,5 +23,39 @@ $h = 1+2+3;
 echo $h;
 */
 //Lesson 3. Task 5.
+//#1
+/*
+$a = 10;
+$b = 2;
+echo $a+$b;
+echo $a-$b;
+echo $a*$b;
+echo $a/$b;
+*/
+//#2
+/*
+$c = 10;
+$d = 5;
+$result = $c+$d;
+echo $result;
+*/
+//#3
+/*
+$a = 1;
+$b = 2;
+$c = 3;
+echo $a+$b+$c;
+*/
+//#4
+/*
+$a=10;
+$b=5;
+$c=$a-$b;
+echo $c;
+$d=7;
+$result=$c*$d;
+echo $result;
+*/
+//Lesson 3. Task 6.
 
 
