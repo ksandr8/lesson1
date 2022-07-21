0981307d19edb2458496a63ce570c2a0ebcdc5cb
ksandr8 <1@bag7!A>
2022-07-21 16:55:12 +0300

    Lesson 3. Task 4.

--- code.php
+++ code.php
@@ -5,10 +5,23 @@
 //echo '12345'; // вивід 12345
 //echo 12345;
 //var_dump('12345'); // вивід 12345 і ще деяку корисну інфу
-//lesson 3. Task 3$
+//lesson 3. Task 3
 //$a = 10;
 //$a = 20;
 //echo $a;
 //lesson 3. Task 4;
+/*
+$a = 1+2;
+echo $a;
+$b = 3-2;
+echo $b;
+$c = 3*2;
+echo $c;
+$d = 4/2;
+echo $d;
+$h = 1+2+3;
+echo $h;
+*/
+//Lesson 3. Task 5.
 
 
