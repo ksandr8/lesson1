f7e799fb9f740bfda305838a70423bce3370cc76
ksandr8 <1@bag7!A>
2022-07-28 16:11:08 +0300

    Lesson 4. Task 12.

--- lesson4.php
+++ lesson4.php
@@ -296,3 +296,120 @@ if (!($num1>=0 or $num2 <= 10)){
 }
 */
 //Lesson 4. Task 12.
+#1
+/*
+$test = true;
+if ($test===true){
+    echo 'True';
+}else{
+    echo 'False';
+}
+*/
+#2
+/*
+$test = true;
+if ($test===false){
+    echo 'True';
+}else{
+    echo 'False';
+}
+*/
+#3
+/*
+$test =1;
+if ($test=true){
+    echo '+';
+}else {
+    echo '-';
+}
+*/
+#6
+/*
+$test =1;
+if ($test!=true){
+    echo '+';
+}else {
+    echo '-';
+}
+*/
+#7
+/*
+$test ='';
+if ($test==false){
+    echo '+';
+}else {
+    echo '-';
+}
+*/
+#8
+/*
+test;
+if (test==true){
+    echo '+';
+}else{
+    echo '-';
+}
+*/
+/*
+$test = true;
+if (!$test){
+    echo '+';
+}else{
+    echo '-';
+
+}
+*/
+/*
+$test = true;
+if ($test!=true){
+    echo '+';
+}else{
+    echo '-';
+}
+*/
+#9
+/*
+$test  = true;
+if ($test){
+    echo '+';
+}else{
+    echo '-';
+}
+*/
+#10
+/*
+$test = true;
+if (!$test){
+    echo '+';
+}else{
+    echo '-';
+}
+*/
+#11
+/*
+$test   = true;
+if (!$test){
+    echo '+';
+}else{
+    echo '-';
+}
+*/
+#12
+/*
+$test=true;
+if ($test){
+    echo '+';
+}else{
+    echo '-';
+}
+*/
+#13
+/*
+$test=3;
+if ($test){
+    echo '+';
+}else{
+    echo '-';
+}
+*/
+//Lesson 4. Task 13
