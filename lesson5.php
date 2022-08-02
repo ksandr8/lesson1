8e46fed90fe1db8083ecccc3ce20a119b5340084
ksandr8 <1@bag7!A>
2022-08-02 14:58:01 +0300

    Lesson 5. Task 47.

--- lesson5.php
+++ lesson5.php
@@ -255,3 +255,25 @@ for ($i=100;$i>=0;$i--){
 }
 */
 //Lesson 5. Task 47.
+/*
+$result = 0;
+for ($i = 1; $i <= 20; $i++) {
+    $result += $i;
+}
+echo $result;
+*/
+/*
+$result = 0;
+for ($i = 2; $i <= 100; $i += 2) {
+    $result = $result+$i;
+}
+echo $result;
+*/
+/*
+$result = 0;
+for ($i = 1; $i <= 99; $i += 2) {
+    $result = $result+$i;
+}
+echo $result;
+*/
+//Lesson 5. Task 48.
