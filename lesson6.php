2031f9ced995e6c76765cae05eac8ca46f607c51
ksandr8 <1@bag7!A>
2022-08-10 21:21:18 +0300

    Lesson 6. Change. Home task.

--- lesson6.php
+++ lesson6.php
@@ -1,3 +1,54 @@
 <?php
+include "lesson6.html";
+/*
+$a = [];
+
+for ($i = 0; $i < 100; $i++) {
+    $a[] = rand(15, 1000000);
+}
+
+echo "<br>";
+echo "Максимальне число: " . max($a);
+echo "<br>";
+echo "Мінімальне число: " . min($a);
+
+$min = $a[0];
+$max = $a[0];
+foreach ($a as $key => $value) {
+    if ($value > $max) {
+        $max = $value;
+    }
+    if ($value < $min) {
+        $min = $value;
+    }
+}
+echo "<br>";
+echo "Максимальне число вручну: " . $max;
+echo "<br>";
+echo "Мінімальне число вручну: " . $min;
+*/
+/*
+$city = "ДніПро";
+$low = mb_strtolower($city);
+$up = mb_convert_case($low, MB_CASE_TITLE,"UTF-8");
+$num = mb_strlen($up);
+echo $up;
+echo "<br>";
+echo $num;
+*/
+/*
+$a =" 12-03-15";
+$b = ltrim($a);
+$num = mb_strlen($a);
+$num1 = mb_strlen($b);
+echo $a;
+echo "<br>";
+echo $b;
+echo "<br>";
+echo $num;
+echo "<br>";
+echo $num1;
+*/
+
 
 
