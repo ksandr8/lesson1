62a89df0f30fc88d87489b6b1c1d49a3652ab298
ksandr8 <1@bag7!A>
2022-08-30 23:15:29 +0300

    Login form change.

--- form login/index.php
+++ form login/index.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 include "AuthFunction.php";
+if (empty($_POST['login']) && empty($_POST['password'])) {
+    echo "Введіть логін та пароль";
+
+}
 if (isset($_POST['login'], $_POST['password']) && !empty($_POST['login']) && !empty($_POST['password'])) {
     $login = $_POST['login'];
     $password = $_POST['password'];
@@ -9,15 +13,14 @@ if (isset($_POST['login'], $_POST['password']) && !empty($_POST['login']) && !em
             echo "Login or password incorect";
         } else {
             $_SESSION['user'] = login($login, $password);
-            $user = $_SESSION['user']['login'];
-            $role = $_SESSION['user']['role'];
-            echo "Привіт $user <br> Роль: $role <br>";
-            echo "Вітаю з авторизацією <br>";
-            include "page1.php";
+            header("Location: page1.php");
             exit();
         }
+
     }
+
 }
+
 ?>
 <form action="" method="post">
     Login:
--- form login/page1.php
+++ form login/page1.php
@@ -1,8 +1,15 @@
 <?php
-if (empty($_GET['submit'])){
-  unset($_SESSION['user']);
-  session_destroy();
+if (isset($_GET['logout']) && !empty($_GET['logout'])) {
+    session_start();
+    header("Location: index.php");
+    session_destroy();
 }
+session_start();
+$user = $_SESSION['user']['login'];
+$role = $_SESSION['user']['role'];
+echo "Привіт $user <br> Роль: $role <br>";
+echo "Вітаю з авторизацією <br>";
+echo "<br>";
 ?>
 <form action="" method="get">
     <input type="submit" name="logout" value="Logout">
